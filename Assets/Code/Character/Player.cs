222fced5e6d8bd8a329bcb25e79ca49aa7ce36c0
Bizert Alexey <UltraBizert@gmail.com>
2017-11-07 17:21:42 +0700

    Player movement.

---
+++ Assets/Code/Character/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float MoveSpeed = 1;
+    public float RotationSpeed = 1;
+    private Vector2 moveInput;
+
+    void Update ()
+    {
+        GetInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetInputs()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+    }
+
+    private void Move()
+    {
+        transform.Translate(0, 0, moveInput.y * MoveSpeed);
+        transform.Rotate(0, moveInput.x * RotationSpeed, 0);
+    }
+}
